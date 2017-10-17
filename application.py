103b7e96785fdde65e35ee92449bc56a5b2f9a69
paulmoliva <paulmoliva@gmail.com>
2017-10-17 09:28:29 -0800

    init

---
+++ application.py
@@ -0,0 +1,14 @@
+import flask
+import os
+
+application = flask.Flask(__name__)
+
+application.secret_key = os.getenv('SECRET_KEY') or 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+@application.route('/')
+def index():
+    return flask.render_template('home.html')
+
+if __name__ == '__main__':
+    application.run(debug=True)
