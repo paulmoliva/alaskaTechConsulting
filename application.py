5a9f685cd57084b8c04c1e8e645f56fab4dd560a
paulmoliva <paulmoliva@gmail.com>
2017-10-17 10:39:55 -0800

    add services and google analytics.

--- application.py
+++ application.py
@@ -10,5 +10,9 @@ application.secret_key = os.getenv('SECRET_KEY') or 'A0Zr98j/3yX R~XHH!jmN]LWX/,
 def index():
     return flask.render_template('home.html')
 
+@application.route('/services')
+def services():
+    return flask.render_template('services.html')
+
 if __name__ == '__main__':
     application.run(debug=True)
